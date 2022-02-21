5a3247e8523aca4513d47841058ebf3ea33428f3
Matthew Egan <mnegan93@gmail.com>
2022-02-21 21:36:29 +1300

    Fetch users and display raw data on page

--- src/UsersPage.js
+++ src/UsersPage.js
@@ -1,7 +1,29 @@
-function UsersPage(props) {
+import {useState, useEffect} from 'react';
+
+function UsersPage() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            const response = await fetch('https://random-data-api.com/api/users/random_user?size=10');
+            const usersData = await response.json();
+            setUsers(usersData);
+        };
+        fetchUsers();
+    }, []);
+    // The empty array [] means
+    // this useEffect will run once
+
     return (
     <div>
         <h1>UsersPage</h1>
+        <ul>
+        {users.map(user => (
+          <li >
+            {user.first_name}
+          </li>
+        ))}
+      </ul>
     </div>
     );
 }
