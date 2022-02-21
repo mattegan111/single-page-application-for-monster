fcd5579300392e7d0ed54da6cbe9fc297ebf112b
Matthew Egan <mnegan93@gmail.com>
2022-02-21 17:37:11 +1300

    Add sidebar with no functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   const loggedUser = 'Monster';
 
   return (
     <div className="App">
+      <Sidebar />
       <h1>Welcome {loggedUser}</h1>
     </div>
   );
---
+++ src/Sidebar.js
@@ -0,0 +1,10 @@
+function Sidebar(props) {
+    return (
+    <div>
+        <h1>Fake Link 'Dashboard'</h1>
+        <h1>Fake Link 'Users'</h1>
+    </div>
+    );
+}
+
+export default Sidebar;
