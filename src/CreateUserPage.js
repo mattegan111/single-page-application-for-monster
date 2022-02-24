4ad0a6fb3340cf396714542cb178cde67590fac9
Matthew Egan <mnegan93@gmail.com>
2022-02-25 00:24:35 +1300

    Add submittable form

--- src/CreateUserPage.js
+++ src/CreateUserPage.js
@@ -1,53 +1,113 @@
+import { useState } from 'react';
+
 function CreateUserPage() {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [dateOfBirth, setDateOfBirth] = useState('');
+    const [creditCard, setCreditCard] = useState('');
+    const [title, setTitle] = useState('');
+    const [plan, setPlan] = useState('');
+
+    function handleSubmit(e){
+        e.preventDefault();
+        const user = { firstName, lastName, email, dateOfBirth, creditCard, title, plan };
+
+        fetch('/dummy-post-url', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(user)
+        }).then(() => {
+            console.log(user);
+        });
+    }
+
     return (
         <>
             <div>
                 <h1>Create User</h1>
             </div>
             <div>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <label>
                         First Name:
-                        <input type="text" name="first_name" />
+                        <input 
+                            name='first_name' 
+                            type='text' 
+                            value={firstName}
+                            onChange={e => setFirstName(e.target.value)} 
+                        />
                     </label>
                     <label>
                         Last Name:
-                        <input type="text" name="last_name" />
+                        <input 
+                            name='last_name' 
+                            type='text' 
+                            value={lastName}
+                            onChange={e => setLastName(e.target.value)} 
+                        />
                     </label>
                     <label>
                         Email:
-                        <input type="text" name="email" />
+                        <input 
+                            name='email' 
+                            type='text' 
+                            value={email}
+                            onChange={e => setEmail(e.target.value)} 
+                        />
                     </label>
                     <label>
                         Date of Birth:
-                        <input type="text" name="date_of_birth" />
+                        <input 
+                            name='date_of_birth' 
+                            type='text' 
+                            value={dateOfBirth}
+                            onChange={e => setDateOfBirth(e.target.value)} 
+                        />
                     </label>
                     <label>
                         Credit Card:
-                        <input type="text" name="cc_number" />
+                        <input 
+                            name='credit_card' 
+                            type='text' 
+                            value={creditCard}
+                            onChange={e => setCreditCard(e.target.value)} 
+                        />
                     </label>
                     <label>
                         Title:
-                        <input type="text" name="employment.title" />
+                        <input 
+                            name='employment.title' 
+                            type='text' 
+                            value={title}
+                            onChange={e => setTitle(e.target.value)} 
+                        />
                     </label>
                     <label>
                         Plan:
-                        <input type="text" name="subscription.plan" />
+                        <input 
+                            name="subscription.plan" 
+                            type='text' 
+                            value={plan}
+                            onChange={e => setPlan(e.target.value)} 
+                        />
+                        {/* <select 
+                            name="subscription.plan" 
+                            onChange={setPlan}>
+                            <option value="free-trial">Free Trial</option>
+                            <option value="student">Student</option>
+                            <option value="essential">Essential</option>
+                            <option value="standard">Standard</option>
+                            <option value="basic">Basic</option>
+                            <option value="professional">Professional</option>
+                            <option value="premium">Premium</option>
+                            <option value="bronze">Bronze</option>
+                            <option value="silver">Silver</option>
+                            <option value="gold">Gold</option>
+                            <option value="diamond">Diamond</option>
+                            <option value="platinum">Platinum</option>
+                        </select> */}
                     </label>
-                    <select>
-                        <option value="free-trial">Free Trial</option>
-                        <option value="student">Student</option>
-                        <option value="essential">Essential</option>
-                        <option value="standard">Standard</option>
-                        <option value="basic">Basic</option>
-                        <option value="professional">Professional</option>
-                        <option value="premium">Premium</option>
-                        <option value="bronze">Bronze</option>
-                        <option value="silver">Silver</option>
-                        <option value="gold">Gold</option>
-                        <option value="diamond">Diamond</option>
-                        <option value="platinum">Platinum</option>
-                    </select>
                     <input type="submit" value="Submit" />
                 </form>
             </div>
